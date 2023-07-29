4d0dd3bf991f10dcc7fde7220647fe459596d47f
Peter Collinson <11645080+pcollinson@users.noreply.github.com>
2023-07-29 13:22:53 +0100

    Fix tty install shell scripts

--- tty/sys/step3.sh
+++ tty/sys/step3.sh
@@ -12,63 +12,63 @@ if -r $1/dmr/orig/dc.c goto dcinst
 echo "No backup for "$1"/dmr/dc.c - not installed"
 goto dcend
 : dcinst
-echo cp sys/dc.c $1/dmr/dc.c
-cp sys/dc.c $1/dmr/dc.c
+echo cp dc.c $1/dmr/dc.c
+cp dc.c $1/dmr/dc.c
 : dcend
 
 if -r $1/dmr/orig/dh.c goto dhinst
 echo "No backup for "$1"/dmr/dh.c - not installed"
 goto dhend
 : dhinst
-echo cp sys/dh.c $1/dmr/dh.c
-cp sys/dh.c $1/dmr/dh.c
+echo cp dh.c $1/dmr/dh.c
+cp dh.c $1/dmr/dh.c
 : dhend
 
 if -r $1/dmr/orig/dhdm.c goto dhdminst
 echo "No backup for "$1"/dmr/dhdm.c - not installed"
 goto dhdmend
 : dhdminst
-echo cp sys/dhdm.c $1/dmr/dhdm.c
-cp sys/dhdm.c $1/dmr/dhdm.c
+echo cp dhdm.c $1/dmr/dhdm.c
+cp dhdm.c $1/dmr/dhdm.c
 : dhdmend
 
 if -r $1/dmr/orig/kl.c goto klinst
 echo "No backup for "$1"/dmr/kl.c - not installed"
 goto klend
 : klinst
-echo cp sys/kl.c $1/dmr/kl.c
-cp sys/kl.c $1/dmr/kl.c
+echo cp kl.c $1/dmr/kl.c
+cp kl.c $1/dmr/kl.c
 : klend
 
 if -r $1/dmr/orig/tty.c goto ttyinst
 echo "No backup for "$1"/dmr/tty.c - not installed"
 goto ttyend
 : ttyinst
-echo cp sys/tty.c $1/dmr/tty.c
-cp sys/tty.c $1/dmr/tty.c
+echo cp tty.c $1/dmr/tty.c
+cp tty.c $1/dmr/tty.c
 : ttyend
 
 if -r $1/ken/orig/sysent.c goto sysentinst
 echo "No backup for "$1"/ken/sysent.c - not installed"
 goto sysentend
 : sysentinst
-echo cp sys/sysent.c $1/ken/sysent.c
-cp sys/sysent.c $1/ken/sysent.c
+echo cp sysent.c $1/ken/sysent.c
+cp sysent.c $1/ken/sysent.c
 : sysentend
 
 if -r $1/conf/orig/m40.s goto m40inst
 echo "No backup for "$1"/conf/m40.s - not installed"
 goto m40end
 : m40inst
-echo cp sys/m40.s $1/conf/m40.s
-cp sys/m40.s $1/conf/m40.s
+echo cp m40.s $1/conf/m40.s
+cp m40.s $1/conf/m40.s
 : m40end
 
 if -r $1/orig/tty.h goto ttyhinst
 echo "No backup for "$1"/tty.h - not installed"
 goto ttyhend
 : ttyhinst
-echo cp sys/tty.h $1/tty.h
-cp sys/tty.h $1/tty.h
+echo cp tty.h $1/tty.h
+cp tty.h $1/tty.h
 : ttyhend
 exit
--- tty/sys/step4.sh
+++ tty/sys/step4.sh
@@ -13,61 +13,61 @@ if -r $1/dmr/dc.c goto dcdiff
    echo "$1/dmr/dc.c not installed"
    goto dcfin
 : dcdiff
-cmp sys/dc.c $1/dmr/dc.c
+cmp dc.c $1/dmr/dc.c
 : dcfin
 
 if -r $1/dmr/dh.c goto dhdiff
    echo "$1/dmr/dh.c not installed"
    goto dhfin
 : dhdiff
-cmp sys/dh.c $1/dmr/dh.c
+cmp dh.c $1/dmr/dh.c
 : dhfin
 
 if -r $1/dmr/dhdm.c goto dhdmdiff
    echo "$1/dmr/dhdm.c not installed"
    goto dhdmfin
 : dhdmdiff
-cmp sys/dhdm.c $1/dmr/dhdm.c
+cmp dhdm.c $1/dmr/dhdm.c
 : dhdmfin
 
 if -r $1/dmr/kl.c goto kldiff
    echo "$1/dmr/kl.c not installed"
    goto klfin
 : kldiff
-cmp sys/kl.c $1/dmr/kl.c
+cmp kl.c $1/dmr/kl.c
 : klfin
 
 if -r $1/dmr/tty.c goto ttydiff
    echo "$1/dmr/tty.c not installed"
    goto ttyfin
 : ttydiff
-cmp sys/tty.c $1/dmr/tty.c
+cmp tty.c $1/dmr/tty.c
 : ttyfin
 
 if -r $1/ken/sysent.c goto sysentdiff
    echo "$1/ken/sysent.c not installed"
    goto sysentfin
 : sysentdiff
-cmp sys/sysent.c $1/ken/sysent.c
+cmp sysent.c $1/ken/sysent.c
 : sysentfin
 
 if -r $1/conf/m40.s goto m40diff
    echo "$1/conf/m40.s not installed"
    goto m40fin
 : m40diff
-cmp sys/m40.s $1/conf/m40.s
+cmp m40.s $1/conf/m40.s
 : m40fin
 
 if -r $1/tty.h goto ttyhdiff
    echo "$1/tty.h not installed"
    goto ttyhfin
 : ttyhdiff
-cmp sys/tty.h $1/tty.h
+cmp tty.h $1/tty.h
 : ttyhfin
 
 if -r $1/deftty.h goto defttyhdiff
    echo "$1/deftty.h not installed"
    goto defttyhfin
 : defttyhdiff
-cmp sys/deftty.h $1/deftty.h
+cmp deftty.h $1/deftty.h
 : defttyhfin
